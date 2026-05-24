93ae6e1e0d60362f82bce863a9769d132acabda3
maus911 <manan801p@gmail.com>
2026-05-24 15:11:36 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> visit;
+
+        while (visit.emplace(n).second) 
+        {
+            visit.insert(n);
+            n = sumOfSquares(n);
+            if (n == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    int sumOfSquares(int n) 
+    {
+        int output = 0;
+
+        while (n > 0) 
+        {
+            int digit = n % 10;
+            digit = digit * digit;
+            output += digit;
+            n /= 10;
+        }
+        return output;
+    }
+};
