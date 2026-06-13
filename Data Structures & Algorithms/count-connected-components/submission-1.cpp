bc4e8654656e922c25decdc61e73de0b033a70c7
maus911 <manan801p@gmail.com>
2026-06-13 20:03:01 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    unordered_set<int> visited;
+
+    void dfs(vector<vector<int>>& graph, int node) 
+    {
+        if (!visited.emplace(node).second)
+            return;
+
+        for (int neighbour : graph[node])
+            dfs(graph, neighbour);
+    }
+
+public:
+    int countComponents(int n, const vector<vector<int>>& edges) 
+    {
+        vector<vector<int>> graph(n);
+
+        for (auto& e : edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+
+        int components = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited.count(i)) {
+                components++;
+                dfs(graph, i);
+            }
+        }
+
+        return components;
+    }
+};
