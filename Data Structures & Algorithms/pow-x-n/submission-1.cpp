399961942130bd5f7648d81393ba558350ea9a6d
maus911 <manan801p@gmail.com>
2026-07-31 19:46:56 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (x == 0) return 0;
+        if (n == 0) return 1;
+
+        double res = 1;
+        int pow = abs(n);
+
+        while(pow > 0)
+        {
+            if (pow & 1)  res *= x;
+
+            x *= x;
+            pow >>= 1;
+        }
+
+        return (n > 0) ? res : 1 / res;
+    }
+};
